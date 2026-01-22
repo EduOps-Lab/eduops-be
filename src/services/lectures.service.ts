8d99ad6cd4ebbaf9bc9fc1169c11820842ba5378
rklpoi5678 <rklpoi5678@naver.com>
2026-01-23 01:01:45 +0900

    feat: find all and  detail service

--- src/services/lectures.service.ts
+++ src/services/lectures.service.ts
@@ -1,7 +1,10 @@
 import { Lecture } from '../generated/prisma/client.js';
 import { NotFoundException } from '../err/http.exception.js';
 import { LecturesRepository } from '../repos/lectures.repo.js';
-import { CreateLectureDto } from '../validations/lectures.validation.js';
+import {
+  CreateLectureDto,
+  GetLecturesQueryDto,
+} from '../validations/lectures.validation.js';
 
 export class LecturesService {
   private lecturesRepository: LecturesRepository;
@@ -23,6 +26,42 @@ export class LecturesService {
     }
 
     const lecture = await this.lecturesRepository.create(data);
+
+    return lecture;
+  }
+
+  /** 강의 리스트 조회 */
+  async getLectures(query: GetLecturesQueryDto): Promise<{
+    lectures: Lecture[];
+    nextCursor: string | null;
+  }> {
+    const { cursor, limit } = query;
+
+    const lectures = await this.lecturesRepository.findMany({
+      cursor,
+      limit,
+    });
+
+    const hasNextPage = lectures.length > limit;
+
+    const resultLectures = hasNextPage ? lectures.slice(0, limit) : lectures;
+
+    // nextCursor 계산: limit보다 많이 조회되면 마지막 항목을 제거하고 그 ID를 커서로 사용
+    const nextCursor = hasNextPage ? lectures[limit - 1].id : null;
+
+    return {
+      lectures: resultLectures,
+      nextCursor: nextCursor,
+    };
+  }
+
+  /** 강의 개별 조회 */
+  async getLectureById(id: string): Promise<Lecture> {
+    const lecture = await this.lecturesRepository.findByIdWithRelations(id);
+
+    if (!lecture) {
+      throw new NotFoundException(`강의를 찾을 수 없습니다. (ID: ${id})`);
+    }
 
     return lecture;
   }
