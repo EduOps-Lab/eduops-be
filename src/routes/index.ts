f6baa5976a6a483399b60697c3665855703e83c5
rklpoi5678 <rklpoi5678@naver.com>
2026-01-23 17:48:52 +0900

    feat: add enrollments route to API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import lecturesRouter from './mgmt/v1/lectures.route.js';
+import enrollmentsRouter from './svc/v1/enrollments.route.js';
 
 export const router = express.Router();
 
@@ -14,5 +15,6 @@ router.get('/', (req, res) => {
 
 // API 라우트 등록
 router.use('/api/mgmt/v1/lectures', lecturesRouter);
+router.use('/api/svc/v1/enrollments', enrollmentsRouter);
 
 export default router;
